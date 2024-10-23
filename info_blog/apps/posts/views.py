54904923cdcd4213fb621bdf0068bfa4ecf4ed02
Emanuel Gomila <gomilaemanuel@gmail.com>
2024-10-23 16:15:11 -0300

    primera vista con publicaciones

--- info_blog/apps/posts/views.py
+++ info_blog/apps/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def home(request):
+    """
+    Funcion que retorna la vista de index.html y carga en el documento los
+    datos de las publicaciones traidas del modelo Post
+    """
+    posts = Post.objects.filter(activo=True)
+    context = {'posts': posts}
+    return render(request, 'index.html', context)
