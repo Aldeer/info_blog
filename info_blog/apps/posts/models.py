11c274252de627f488f71ead10c557b567ee9641
Emanuel Gomila <gomilaemanuel@gmail.com>
2024-10-23 16:11:13 -0300

    modelo de Post

--- info_blog/apps/posts/models.py
+++ info_blog/apps/posts/models.py
@@ -1,2 +1,22 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+
+class Post(models.Model):
+    """
+    Clase modelo de publicacion
+    """
+
+    titulo = models.CharField('Titulo', max_length=200, blank=False, null=False)
+    descripcion = models.CharField('Descripcion', max_length=200, blank=False, null=False)
+    contenido = RichTextField('Contenido')
+    portada = models.URLField('Portada', max_length=255)
+    activo = models.BooleanField('Publicacion Activa', default=True)
+    creado = models.DateTimeField('Fecha de creación', auto_now=False, auto_now_add=True)
+    modificado = models.DateTimeField('Fecha de ultima modificación', auto_now=True, auto_now_add=False)
+
+    class Meta:
+        verbose_name = 'Publicacion'
+        Verbose_name_plural = 'Publicaciones'
+
+    def __str__(self):
+        return f'{self.titulo}'
